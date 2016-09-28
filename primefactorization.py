089423c0db90eb30bdf259dbf7d1888ae6fb7c39
Dylan Moore <hosnappp@gmail.com>
2016-09-28 17:03:11 -0400

    Update primefactorization.py

--- primefactorization.py
+++ primefactorization.py
@@ -22,7 +22,8 @@ def is_prime(i):
     #the square of a factor that we know isn't even due to factorize(x)... the only exception to this method being 
     #when the number we're proving is a perfect square & prime of the factor.
     #
-    #example: sqrt(14)=3.74 but has a prime factor of 7 (7^2=14) which is larger than the sqrt.
+    #example: sqrt(14)=3.74 but has a prime factor of 7 (7^2=14) which is larger than the sqrt. this however is ruled
+    #out by the first chunk, just wanted to be mathematically rigorous in the comment
     #a.k.a. the sieve of eratosthenes
     print(all(i%n for n in range(3,int(sqrt(i))+1,2)))
 
